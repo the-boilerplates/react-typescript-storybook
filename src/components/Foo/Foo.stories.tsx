fdae37a6c0ccce6020fb8c6b6f8d1ee25d8e8db9
maddhruv <dhruvjainpenny@gmail.com>
2020-06-26 16:32:42 +0530

    feat: initial boilerplate

---
+++ src/components/Foo/Foo.stories.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import { storiesOf } from "@storybook/react";
+
+import Foo from "./Foo";
+
+storiesOf("Foo", module).add("Story", () => <Foo />);
---
+++ src/components/Foo/Foo.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Foo: React.FC = () => {
+  return <div>Bar</div>;
+};
+
+export default Foo;
